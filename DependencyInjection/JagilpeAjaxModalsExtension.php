a648dc44385dd9b1e9bbe63fe3ea3f317b5c0925
Maxim Peshehonov <mpeshehonov@users.noreply.github.com>
2020-05-26 12:02:54 +0300

    Update JagilpeAjaxModalsExtension.php

--- DependencyInjection/JagilpeAjaxModalsExtension.php
+++ DependencyInjection/JagilpeAjaxModalsExtension.php
@@ -2,10 +2,11 @@
 
 namespace Jagilpe\AjaxModalsBundle\DependencyInjection;
 
+use Sg\DatatablesBundle\DependencyInjection\Configuration;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
-use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 /**
  * This is the class that loads and manages your bundle configuration
@@ -24,7 +25,7 @@ class JagilpeAjaxModalsExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
 }
